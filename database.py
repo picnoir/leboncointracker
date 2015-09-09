62c745aa7281a824a231ea04fe87e0a707543076
Baylac-Jacqué Félix <baylac.felix@gmail.com>
2015-09-09 13:46:07 +0000

    Saving datetime of posts.

--- database.py
+++ database.py
@@ -1,20 +1,25 @@
 import sqlite3
+from datetime import datetime
 
 class Appartment:
     """This class contains the data describing an Appartment"""
-    def __init__(self, url, price, surface, zip_code, nb_rooms=None, type=None):
+    def __init__(self, url, price, surface, zip_code,
+                 nb_rooms=None, type=None, post_date=datetime.now()):
         self.url = url
         self.price = price
         self.surface = surface
         self.zip_code = zip_code
         self.nb_rooms = nb_rooms
         self.type = type
+        self.post_date = post_date
 
     def save(self, cursor):
         request_params = (self.price, self.surface, self.zip_code,
-                          self.nb_rooms, self.type, self.url)
-        cursor.execute('''INSERT INTO appartments (price, surface, zip_code,
-                        nb_rooms, type, url) VALUES (?,?,?,?,?,?)''', request_params)
+                          self.nb_rooms, self.type, self.url, self.post_date)
+        cursor.execute('''INSERT INTO appartments (price, surface,
+                          zip_code, nb_rooms, type, url, post_date)
+                          VALUES (?,?,?,?,?,?,?)''',
+                       request_params)
 
     @staticmethod
     def createDB(dbName):
@@ -23,9 +28,12 @@ class Appartment:
         cursor = connection.cursor()
         cursor.execute('''CREATE TABLE appartments
                             (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-                            price REAL NOT NULL, surface REAL NOT NULL,
-                            zip_code INT NOT NULL, url text NOT NULL,
-                            nb_rooms text, type text)''')
+                            price INT NOT NULL, surface INT NOT NULL,
+                            zip_code INT NOT NULL, url VARCHAR(200) NOT NULL,
+                            nb_rooms VARCHAR(10), type VARCHAR(30),
+                            post_date DATETIME)''')
+        cursor.execute('''CREATE UNIQUE INDEX appartmentsUrl
+                       on appartments(url)''')
         connection.commit()
         connection.close()
 
